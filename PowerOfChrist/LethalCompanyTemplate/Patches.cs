8137d00306fa633994b43c5c8bbaa9f4e91fb23c
Lemo <jackniems@gmail.com>
2024-09-24 15:18:23 -0700

    Cleaned up code

--- PowerOfChrist/LethalCompanyTemplate/Patches.cs
+++ PowerOfChrist/LethalCompanyTemplate/Patches.cs
@@ -10,12 +10,17 @@ namespace PowerOfChrist
     [HarmonyPatch]
     class Patches
     {
+        /// <summary>
+        /// Prepares the Hud Flasher for this game upon the creation of the player
+        /// </summary>
         [HarmonyPatch(typeof(GameNetcodeStuff.PlayerControllerB), "Awake")]
-        [HarmonyPrefix]
-        internal static void PlayerControllerB_Awake_Prefix(ref GameNetcodeStuff.PlayerControllerB __instance)
+        [HarmonyPostfix]
+        internal static void PlayerControllerB_Awake_Postfix(ref GameNetcodeStuff.PlayerControllerB __instance)
         {
             try
             {
+                // If we haven't assigned the hud flasher yet, create and reference it
+                // NOTE: The mod doesn't account for the lobby restarting, and will attempt to access a deleted GameObject due to that
                 if (Plugin.hudFlasher == null)
                 {
                     Plugin.hudFlasher = GetOrAddHUDObject().GetComponent<HudFlasher>();
@@ -23,28 +28,24 @@ namespace PowerOfChrist
                     Plugin.Logger.LogInfo("Created flasher with " + Plugin.hudFlasher.textures.Length + " textures");
                 }
 
-                if (__instance == GameNetworkManager.Instance.localPlayerController)
-                {
-                    Plugin.hudFlasher.BeginFlash();
-                }
+                // Attempt a flash sequence to fully initialize the flasher object
+                BeginFlasher();
             }
             catch (Exception e) 
             {
                 Plugin.Logger.LogError(e);
             }
-            
         }
 
+        // Trigger the flash check upon taking damage
         [HarmonyPatch(typeof(GameNetcodeStuff.PlayerControllerB), "DamagePlayer")]
         [HarmonyPostfix]
         internal static void PlayerControllerB_DamagePlayer_Postfix(ref GameNetcodeStuff.PlayerControllerB __instance, int damageNumber, bool hasDamageSFX, bool callRPC, CauseOfDeath causeOfDeath, int deathAnimation, bool fallDamage, Vector3 force)
         {
-            if (__instance == GameNetworkManager.Instance.localPlayerController)
-            {
-                Plugin.hudFlasher.BeginFlash();
-            }
+            BeginFlasher();
         }
 
+        // Stop the flash sequence upon death, the player is in the hands of the lord now :)
         [HarmonyPatch(typeof(GameNetcodeStuff.PlayerControllerB), "KillPlayer")]
         [HarmonyPostfix]
         internal static void PlayerControllerB_KillPlayer_Postfix(ref GameNetcodeStuff.PlayerControllerB __instance, Vector3 bodyVelocity, bool spawnBody = true, CauseOfDeath causeOfDeath = CauseOfDeath.Unknown, int deathAnimation = 0, Vector3 positionOffset = default(Vector3))
@@ -52,6 +53,7 @@ namespace PowerOfChrist
             StopFlasher();
         }
 
+        // Stop the flash sequence from looping now that the player is fully restored. May be redundant
         [HarmonyPatch(typeof(StartOfRound), "ReviveDeadPlayers")]
         [HarmonyPostfix]
         internal static void StartOfRound_ReviveDeadPlayers_Postfix()
@@ -59,35 +61,52 @@ namespace PowerOfChrist
             StopFlasher();
         }
 
+        /// <summary>
+        /// Finds the ImageFlasher for this mod and, if it doesn't exist, creates one
+        /// </summary>
+        /// <returns>The GameObject with the component HudFlasher</returns>
         static GameObject GetOrAddHUDObject()
         {
             GameObject canvas = GameObject.Find("Canvas");
-            GameObject rawImageObj = new GameObject("ImageFlasher", typeof(UnityEngine.UI.RawImage), typeof(HudFlasher));
+            GameObject rawImageObj = canvas.transform.find("ImageFlasher");
 
-            rawImageObj.transform.SetParent(canvas.transform);
+            if (rawImageObj != null)
+            {
+                rawImageObj = new GameObject("ImageFlasher", typeof(UnityEngine.UI.RawImage), typeof(HudFlasher));
+                rawImageObj.transform.SetParent(canvas.transform);
+            }
 
             return rawImageObj;
         }
 
+        /// <summary>
+        /// Creates a Texture2D from the image at the given path
+        /// </summary>
+        /// <param name="path">File name including file extension</param>
+        /// <returns>Texture2D of the given image, returns null if image failed to load</returns>
         static Texture2D LoadImageInFolder(string path)
         {
             Texture2D texture = new Texture2D(2, 2);
             byte[] bytes = File.ReadAllBytes(path);
             if (!ImageConversion.LoadImage(texture, bytes))
             {
-                Plugin.Logger.LogWarning(path + " failed to be loaded");
-            }
-            else
-            {
-                Plugin.Logger.LogInfo(path + " successfully loaded");
+                Plugin.Logger.LogWarning(path + " failed to be loaded")
+                return null;
             }
 
+            Plugin.Logger.LogInfo(path + " successfully loaded");
             return texture;
         }
 
+        /// <summary>
+        /// Constructs a list of Texture2D out of image files in the given directory.
+        /// Supports png and jpg.
+        /// </summary>
+        /// <param name="path">The absolute path of the folder containing the images</param>
+        /// <returns>List of Texture2D's, images that failed to load will be null.</returns>
         static Texture2D[] CreateTexturesFromFolder(string path)
         {
-            string[] imagePaths = Directory.GetFiles(path, "*.png");
+            string[] imagePaths = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories).Where(s => s.EndsWith(".png") || s.EndsWith(".jpg"));
             Texture2D[] textures = new Texture2D[imagePaths.Length];
 
             for (int i = 0; i < imagePaths.Length; i++)
@@ -98,12 +117,25 @@ namespace PowerOfChrist
             return textures;
         }
 
-        static void StopFlasher ()
+        /// <summary>
+        /// Attempts to stop the Hud Flashing sequence
+        /// </summary>
+        static bool StopFlasher ()
         {
             if (Plugin.hudFlasher != null)
             {
                 Plugin.hudFlasher.StopFlash();
+                return true;
             }
+            return false;
+        }
+
+        /// <summary>
+        /// Attempts to begin Hud Flashing sequence
+        /// </summary>
+        static bool BeginFlasher ()
+        {
+            Plugin.hudFlasher.BeginFlash();
         }
     }    
 }
